2e42169907f16e6278a1a26bcb5861868cec341f
Faisan333 <azeezfaizan@gmail,com>
2025-04-20 10:33:46 -0700

    New Changes

--- js/custom.js
+++ js/custom.js
@@ -59,4 +59,19 @@ function closemodal(){
 }
 
 
+let copybtn = document.querySelector(".copybtn");
+
+
+function copyIt(){
+  let copyInput = document.querySelector('#copyvalue');
+
+  copyInput.select();
+
+  document.execCommand("copy");
+
+  copybtn.textContent = "COPIED";
+}
+
+
+
 
